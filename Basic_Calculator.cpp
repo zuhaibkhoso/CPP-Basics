9d03c9c34cbbcbcce52059bc33a483a2e0f8e596
Zuhaib Khoso <90856997+zuhaibkhoso@users.noreply.github.com>
2025-05-26 20:47:40 +0500

    Update Basic_Calculator.cpp

--- Basic_Calculator.cpp
+++ Basic_Calculator.cpp
@@ -3,70 +3,50 @@
 using namespace std;
 
 int main() {
-
 	float x, y, result;
 	string input;
 	
 	while(true){
-
 		cout << "What do you want to perform?\n\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n\npress 'Q' to quit\n\nEnter Option: ";
 		cin >> input;
 		
-		
-		if (input == "1"){
-			
+		if (input == "1"){	
 			cout << "Enter 1st Number: ";
 			cin >> x;
-		
 			cout << "Enter 2nd Number: ";
 			cin >> y;
-		
 			result = x + y;
-		
 			cout << "\nFinal Output: " << result << "\n" << endl;
 		}
 		
 			if (input == "2"){
-			
 			cout << "Enter 1st Number: ";
 			cin >> x;
-		
 			cout << "Enter 2nd Number: ";
 			cin >> y;
-		
 			result = x - y;
-		
 			cout << "\nFinal Output: " << result << "\n" << endl;
 		}
 		
 			if (input == "3"){
-			
 			cout << "Enter 1st Number: ";
 			cin >> x;
-		
 			cout << "Enter 2nd Number: ";
 			cin >> y;
-		
 			result = x * y;
-		
 			cout << "\nFinal Output: " << result << "\n" << endl;
 		}
 		
 			if (input == "4"){
-			
 			cout << "Enter 1st Number: ";
 			cin >> x;
-		
 			cout << "Enter 2nd Number: ";
 			cin >> y;
-		
 			result = x / y;
-		
 			cout << "\nFinal Output: " << result << "\n" << endl;
 		}
 		
 		if (input == "Q"){
-			
 			return 0;
 		}
 	}
